bbcce98162f130478fbd19b9ebd5d469d0f193d0
cmelo72 <chris@cloudnetworkz.com>
2012-11-01 21:22:06 -0400

    Updated video with in paving and materials site

--- Hartford.Web/Controllers/HomeController.cs
+++ Hartford.Web/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 namespace Hartford.Web.Controllers
 {
     public class HomeController : Controller
-    {
+    {        
         public ActionResult Index()
         {
             return View();
